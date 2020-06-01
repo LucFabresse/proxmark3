97934fffd9b94aef4ce0b4242f923cc4ef2e70a9
Luc Fabresse <luc.fabresse@gmail.com>
2020-06-01 14:52:16 +0000

    clone script

--- client/cmdscripts/start_hid_scan.sh
+++ client/cmdscripts/start_hid_scan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 screen -dmS pm3 bash -c 'pm3 -i -c "lf hid watch"'
-File_Log=$HOME/.proxmark3/`ls -t $HOME/.proxmark3/ | head -1` 
+File_Log=`ls -t $HOME/.proxmark3/log_* | head -1` 
 echo "Log : $File_Log"
 exec tail -f $File_Log | grep TAG\ ID &
